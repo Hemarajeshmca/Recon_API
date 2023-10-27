11fc37076f4a1c9f4f2f1faac93e82d5b8dac7c1
Hema Jayaraman <hema@flexicodeindia.com>
2023-10-27 12:19:13 +0530

    Final changes

--- Recon_API/Recon_API/Startup.cs
+++ Recon_API/Recon_API/Startup.cs
@@ -16,9 +16,18 @@ namespace Recon_API
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
             services.AddControllers();
             services.Configure<confmodal>(Configuration.GetSection("AppSettings"));
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:42374")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod(); //THIS LINE RIGHT HERE IS WHAT YOU NEED
+                    });
+            });
             //services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
             //services.AddDbContext<ApplicationDbContext>(options =>
             //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
