d4c41587ab662934e5a57822b2aaa3b59401dd1e
vinoth prasath <vinothprasath.s@flexicodeindia.com>
2024-03-06 12:25:43 +0530

    api changes

--- Recon_API/ReconDataLayer/PreprocessService.cs
+++ Recon_API/ReconDataLayer/PreprocessService.cs
@@ -6,23 +6,37 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ReconModels.PreprocessModel;
 using static ReconModels.ReconModel;
+using static ReconModels.theme_model;
 
 namespace ReconServiceLayer
 {
     public class PreprocessService
     {
-        public static DataTable getPreprocessList(UserManagementModel.headerValue headerval, string constring)
+        public static DataTable getPreprocessList(Preprocesslistmodel objpreprocess,UserManagementModel.headerValue headerval, string constring)
         {
             DataTable ds = new DataTable();
             try
             {
                 PreprocessData objqcd = new PreprocessData();
-                ds = objqcd.getPreprocessListData(headerval, constring);
+                ds = objqcd.getPreprocessListData(objpreprocess,headerval, constring);
             }
             catch (Exception e)
             { }
             return ds;
         }
-    }
+		public static DataTable preprocessHeader(preprocessHeadermodel preprocessHeadermodel, UserManagementModel.headerValue headerval, string constring)
+		{
+			DataTable ds = new DataTable();
+			try
+			{
+				PreprocessData objDS = new PreprocessData();
+				ds = objDS.preprocessHeaderdata(preprocessHeadermodel, headerval, constring);
+			}
+			catch (Exception e)
+			{ }
+			return ds;
+		}
+	}
 }
--- Recon_API/Recon_API/Controllers/PreprocessController.cs
+++ Recon_API/Recon_API/Controllers/PreprocessController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using ReconServiceLayer;
 using System.Data;
+using static ReconModels.PreprocessModel;
+using static ReconModels.theme_model;
 using static ReconModels.UserManagementModel;
 
 namespace Recon_API.Controllers
@@ -18,7 +20,7 @@ namespace Recon_API.Controllers
         }
         string constring = "";
         [HttpPost("Preprocesslist")]
-        public IActionResult PreprocessList()
+        public IActionResult PreprocessList(Preprocesslistmodel objpreprocess )
         {
             constring = _configuration.GetSection("Appsettings")["ConnectionStrings"].ToString();
             headerValue header_value = new headerValue();
@@ -31,7 +33,7 @@ namespace Recon_API.Controllers
                 header_value.user_code = getvalue;
                 header_value.lang_code = getlangCode;
                 header_value.role_code = getRoleCode;
-                response = PreprocessService.getPreprocessList( header_value, constring);
+                response = PreprocessService.getPreprocessList(objpreprocess,header_value, constring);
                 var serializedProduct = JsonConvert.SerializeObject(response, Formatting.None);
                 return Ok(serializedProduct);
             }
@@ -40,5 +42,28 @@ namespace Recon_API.Controllers
                 return Problem(title: e.Message);
             }
         }
-    }
+		[HttpPost("preprocessHeader")]
+		public IActionResult preprocessHeader(preprocessHeadermodel preprocessHeadermodel)
+		{
+			constring = _configuration.GetSection("Appsettings")["ConnectionStrings"].ToString();
+			headerValue header_value = new headerValue();
+			DataTable response = new DataTable();
+			try
+			{
+				var getvalue = Request.Headers.TryGetValue("user_code", out var user_code) ? user_code.First() : "";
+				var getlangCode = Request.Headers.TryGetValue("lang_code", out var lang_code) ? lang_code.First() : "";
+				var getRoleCode = Request.Headers.TryGetValue("role_code", out var role_code) ? role_code.First() : "";
+				header_value.user_code = getvalue;
+				header_value.lang_code = getlangCode;
+				header_value.role_code = getRoleCode;
+				response = PreprocessService.preprocessHeader(preprocessHeadermodel, header_value, constring);
+				var serializedProduct = JsonConvert.SerializeObject(response, Formatting.None);
+				return Ok(serializedProduct);
+			}
+			catch (Exception e)
+			{
+				return Problem(title: e.Message);
+			}
+		}
+	}
 }
