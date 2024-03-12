a708914b2ab0104915aab68f7885c087fb45de6d
Hema Jayaraman <hema@flexicodeindia.com>
2024-03-12 11:55:44 +0530

    Api changes

--- Recon_API/Recon_API/Controllers/DatasettoReconController.cs
+++ Recon_API/Recon_API/Controllers/DatasettoReconController.cs
@@ -153,9 +153,11 @@ namespace Recon_API.Controllers
 				var getvalue = Request.Headers.TryGetValue("user_code", out var user_code) ? user_code.First() : "";
 				var getlangCode = Request.Headers.TryGetValue("lang_code", out var lang_code) ? lang_code.First() : "";
 				var getRoleCode = Request.Headers.TryGetValue("role_code", out var role_code) ? role_code.First() : "";
+				var getIpAddress = Request.Headers.TryGetValue("ip_address", out var ip_address) ? role_code.First() : "";
 				header_value.user_code = getvalue;
 				header_value.lang_code = getlangCode;
 				header_value.role_code = getRoleCode;
+				header_value.ip_address = getIpAddress;
 				response = DatasettoReconservice.runProcessdatasetService(objrunProcessdataset, header_value, constring);
 				var serializedProduct = JsonConvert.SerializeObject(response, Formatting.None);
 				return Ok(serializedProduct);
