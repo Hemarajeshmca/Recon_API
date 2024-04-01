7e72912174d59c41d18331e3573b9f95f3394315
vinoth prasath <vinothprasath.s@flexicodeindia.com>
2024-04-01 15:02:09 +0530

    last log in chnages

--- Recon_API/ReconDataLayer/UserManagementService.cs
+++ Recon_API/ReconDataLayer/UserManagementService.cs
@@ -168,6 +168,19 @@ namespace ReconServiceLayer
 				{ }
 				return ds;
 			}
-		}
+            public static DataTable lastlogin_srv(UserManagementModel.getmenumodel objmodel, UserManagementModel.headerValue headerval, string constring)
+            {
+                DataTable ds = new DataTable();
+                try
+                {
+                    UserManagementData objproduct = new UserManagementData();
+                    ds = objproduct.lastlogin_db(objmodel, headerval, constring);
+                }
+                catch (Exception e)
+                { }
+                return ds;
+            }
+            
+        }
     }
 }
