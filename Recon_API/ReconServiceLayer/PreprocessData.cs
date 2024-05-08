0751f3c03a2f1ff6788b86ed49a19258c21c8e96
vinoth prasath <vinothprasath.s@flexicodeindia.com>
2024-05-08 18:14:45 +0530

    look up dataset

--- Recon_API/ReconServiceLayer/PreprocessData.cs
+++ Recon_API/ReconServiceLayer/PreprocessData.cs
@@ -289,5 +289,29 @@ namespace ReconDataLayer
 				return result;
 			}
 		}
+		public DataTable getdatasetReconPreprocessdata(getdataagainsRecon objdatarecon, UserManagementModel.headerValue headerval, string constring)
+		{
+			try
+			{
+				DBManager dbManager = new DBManager(constring);
+				Dictionary<string, Object> values = new Dictionary<string, object>();
+				MySqlDataAccess con = new MySqlDataAccess("");
+				parameters = new List<IDbDataParameter>();
+				parameters.Add(dbManager.CreateParameter("in_recon_code", objdatarecon.in_recon_code, DbType.String));
+				parameters.Add(dbManager.CreateParameter("in_user_code", headerval.user_code, DbType.String));
+				parameters.Add(dbManager.CreateParameter("in_role_code", headerval.role_code, DbType.String));
+				parameters.Add(dbManager.CreateParameter("in_lang_code", headerval.lang_code, DbType.String));
+				ds = dbManager.execStoredProcedure("pr_get_datasetpreprocess", CommandType.StoredProcedure, parameters.ToArray());
+				result = ds.Tables[0];
+				return result;
+			}
+			catch (Exception ex)
+			{
+				CommonHeader objlog = new CommonHeader();
+				objlog.logger("SP:pr_get_datasetpreprocess" + "Error Message:" + ex.Message);
+				objlog.commonDataapi("", "SP", ex.Message, "pr_get_datasetpreprocess", headerval.user_code, constring);
+				return result;
+			}
+		}
 	}
 }
