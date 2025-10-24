63edabf5085eeb1298bb608a05f29298547bc61d
Muthu <muthumanikavel@flexicodeindia.com>
2025-10-24 16:26:42 +0530

    Reapply "Kill Job API"
    This reverts commit 742f7580b03db81c6ce51f31b2cd8419c3d0e83e.

---
+++ Recon_API/ReconModel/KilljobModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReconModels
+{
+    public class KilljobModel
+    {
+        public Int64 processId { get; set; }
+        public string? action { get; set; } = null;
+		public string? userCode { get; set; } = null;
+
+
+	}
+}
